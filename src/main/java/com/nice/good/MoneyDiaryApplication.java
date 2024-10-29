68d0d1c75629722551cce55cd4ccd4ecd69ade84
hsb <242593@IC0-W-N17287.ssg.global>
2024-10-29 11:21:43 +0900

    프로젝트 생성 직후 초기커밋

---
+++ src/main/java/com/nice/good/MoneyDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.nice.good;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nice/good/MoneyDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nice.good;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
